cfb38378aef5ab16be3c1c55ca3557720eb57482
Baldur van Lew <b.van_lew@lumc.nl>
2024-07-02 15:49:57 +0200

    Test file type

--- conanfile.py
+++ conanfile.py
@@ -21,6 +21,7 @@ class CrossOmp(ConanFile):
 
         tc = CMakeToolchain(self, generator=generator)
         if is_apple_os(self):
+            print(f"Working with architecture {arch}")
             proc = subprocess.run(
                 f"arch -{arch} brew --prefix libomp", shell=True, capture_output=True
             )
