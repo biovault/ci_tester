858bcdceac8043947c4b44bc30425749ce1d0f8f
Baldur van Lew <b.van_lew@lumc.nl>
2024-07-02 14:52:21 +0200

    Use explicit arch in conanfile

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,7 @@ from conan.tools.system import package_manager
 from conan.tools.cmake import CMakeToolchain, CMake
 from conan.tools.apple import is_apple_os
 import subprocess
+import platform
 
 class CrossOmp(ConanFile):
 
@@ -14,13 +15,14 @@ class CrossOmp(ConanFile):
     generators = "CMakeDeps"
 
     def generate(self):
+        arch = str(platform.machine())
         if is_apple_os(self):
             generator = "Xcode"
 
         tc = CMakeToolchain(self, generator=generator)
         if is_apple_os(self):
             proc = subprocess.run(
-                "brew --prefix libomp", shell=True, capture_output=True
+                f"arch -{arch} brew --prefix libomp", shell=True, capture_output=True
             )
             prefix_path = f"{proc.stdout.decode('UTF-8').strip()}"
             tc.variables["OpenMP_ROOT"] = prefix_path   
