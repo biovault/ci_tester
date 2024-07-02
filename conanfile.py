a9c7dea47fe541e6ca8d80e72f1aae4d41ecf3d9
Baldur van Lew <b.van_lew@lumc.nl>
2024-07-02 09:59:45 +0200

    Add conan build using CMakeDeps

---
+++ conanfile.py
@@ -0,0 +1,29 @@
+from conan import ConanFile
+from conan.tools.system import package_manager
+from conan.tools.cmake import CMakeToolchain
+from conan.tools.apple import is_apple_os
+import subprocess
+
+class CrossOmp(ConanFile):
+
+    name = "cross"
+    description = "Text cross compiling on macos arm to x86_64"
+    license = "Apache-2.0"
+    url = "https://github.com/biovault/ci_tester.git"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeDeps"
+
+    def generate(self):
+        if is_apple_os(self):
+            generator = "Xcode"
+
+        tc = CMakeToolchain(self, generator=generator)
+        if is_apple_os(self):
+            proc = subprocess.run(
+                "brew --prefix libomp", shell=True, capture_output=True
+            )
+            prefix_path = f"{proc.stdout.decode('UTF-8').strip()}"
+            tc.variables["OpenMP_ROOT"] = prefix_path            
+  
+    def system_requirements(self):
+        package_manager.Brew(self).install(["libomp"], update=True, check=True)
